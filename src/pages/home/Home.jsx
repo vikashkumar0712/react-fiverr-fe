bf1d69351d0cafd92ce8debfdd4a73366816a84a
Zuber Khan <zuberkhan034@gmail.com>
2023-05-08 11:16:56 +0530

    Fixed bugs in search

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import "./Home.scss";
-import React, { useEffect } from "react";
+import React from "react";
 import { cards, projects } from "../../common/data";
 import { HeroSection } from "../../components/hero_section/HeroSection";
 import { TrustedBy } from "../../components/trusted_by/TrustedBy";
@@ -8,12 +8,7 @@ import { CatCard } from "../../components/cat_card/CatCard";
 import { FeaturesPromo } from "../../components/features_promo/FeaturesPromo";
 import { FeaturesBusiness } from "../../components/features_business/FeaturesBusiness";
 import { ProjectCard } from "../../components/project_card/ProjectCard";
-import { newRequest } from "../../utils/request";
 export const Home = () => {
-  useEffect(async () => {
-    await newRequest.get();
-  }, []);
-
   return (
     <div className="home">
       <HeroSection />
